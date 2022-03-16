6f6c21e99d655a9257242e74e25e665e9f26403c
pkzrnvch <pkazarnovich@gmail.com>
2022-03-16 21:16:25 +0500

    bot variable is set outside the loop while True

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ def main():
     fetch_spacex_latest_launch()
     fetch_latest_nasa_epic(nasa_token)
     fetch_nasa_apod(nasa_token)
+    bot = telegram.Bot(token=tg_token)
     photos_to_post = []
     for entry in os.scandir(photo_folder):
         if not entry.name.startswith('.') and entry.is_file():
             photos_to_post.append(entry.path)
     while True:
         photo_to_post = random.choice(photos_to_post)
-        bot = telegram.Bot(token=tg_token)
         with open(photo_to_post, 'rb') as image_to_post:
             bot.send_photo(chat_id=tg_chat_id, photo=image_to_post)
         sleep(seconds_between_posts)
